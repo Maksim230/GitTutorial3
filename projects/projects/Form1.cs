825c3b1cb65bce9cca77a9903a67388d592ffbb8
Maksim Lazutkin <lazutkin.maksim230@mail.ru>
2017-09-09 19:04:06 +0400

    Made some changes

--- projects/projects/Form1.cs
+++ projects/projects/Form1.cs
@@ -16,5 +16,10 @@ namespace projects
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Привет");
+        }
     }
 }
